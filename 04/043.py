b34ac298a4180d3408fa05100c4f1d32b2a72f34
DionicShinobi <dashakozlova17.10@gmail.com>
2024-07-30 23:35:55 +0200

    Split one list into two lists commit

---
+++ 04/043.py
@@ -0,0 +1,12 @@
+import random
+
+# Створюємо список випадкових чисел із випадковою кількістю елементів від 3 до 10
+list_length = random.randint(3, 10)
+random_list = [random.randint(0, 100) for _ in range(list_length)]
+
+print("Початковий список:", random_list)
+
+# Створюємо новий список із першого, третього і другого з кінця елементів
+new_list = [random_list[0], random_list[2], random_list[-2]]
+
+print("Новий список:", new_list)
